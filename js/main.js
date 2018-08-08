3d35d8f0673c547ab097bf75c26f94aeaed76e7e
MopTra <1053978204@qq.com>
2018-08-08 21:41:20 +0800

    自动轮播没动画“

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ sliderdata={
     modmod:0,
     picData:[["./img/2.jpg",'http://www.jd.com'],["./img/3.jpg",'http://www.qq.com'],["./img/1.jpg",'http://www.baidu.com']],
 }
-// person={firstname:"John",lastname:"Doe",age:50,eyecolor:"blue"};
-$('.controller').bind('click',function () {
+setInterval(function () {
     $('.recommandlink').attr({href:sliderdata.picData[sliderdata.modmod][1]});
     $('.recommandphoto').attr({src:sliderdata.picData[sliderdata.modmod][0]});
-    sliderdata.modmod=(sliderdata.modmod+1)%3;
+    sliderdata.modmod=(sliderdata.modmod+1)%sliderdata.picData.length;
     console.log(sliderdata.modmod);
     console.log(sliderdata.picData[sliderdata.modmod][0]);
-})
+},sliderdata.eachtime)
+$('.controller').bind('click',)
 console.log(sliderdata.picData[1][0])
