9b6e76b74b079ce7a87c5ccfda614790e557d0ba
MopTra <1053978204@qq.com>
2018-08-09 22:12:36 +0800

    增加了左右两个切换按钮，但没有实际的功能，也只是纯色

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 sliderdata={
     eachtime:1000,
     changetime:150,
-    nowid:0,
+    nowid:1,
     picData:[["./img/1.jpg",'http://www.jd.com'],["./img/2.jpg",'http://www.qq.com'],["./img/3.jpg","http://www.163.com"],["./img/4.jpg",'http://www.baidu.com'],],
 }
+$('.btn').toggle();//一开始btn是隐藏的
+
 function change(newid) {
     //这个newid不是offset，是第几个图片
     newid=newid-1;
@@ -39,14 +41,18 @@ function mousemovehandler(){
         console.log(event);
         clearInterval(timerid);
         $('.slider').off('mousemove');
+
         //一次就行，以免以后鼠标在里面动的时候影响性能
+        $('.btn').toggle();
+        //改变btn的状态
     })
 }
 mousemovehandler();
-
 $('.slider').mouseout(function (event) {
     console.log(event);
     startswipe();
     mousemovehandler();
     //出来之后在给他绑上事件
+    $('.btn').toggle();
+    //离开的时候也要改变btn的状态
 })
